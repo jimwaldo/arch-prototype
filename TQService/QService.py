2c50260fb57d5b8030ca6dd667afc20aaa3c5e84
Jim Waldo <jiniwaldo@gmail.com>
2013-05-14 16:12:16 -0400

    First implementation of the server, task, TaskStatus, and TaskStatus Proxy

--- TQService/QService.py
+++ TQService/QService.py
@@ -6,6 +6,7 @@ Created on May 12, 2013
 import rpyc
 import uuid
 from collections import deque
+from socket import gethostname
 
 class XTask:
     def __init__(self, destId, data):
@@ -31,22 +32,54 @@ class TaskStatus(object):
         self.progress = 0.0
         
 class TSProxy(TaskStatus):
-    def __init__(self, tqs):
-        self.queueSerice = tqs
+    def __init__(self, taskId, retDest, host, port):
+        super().__init__(taskId, retDest)
+        self.host = host
+        self.port = port
+        self.connected = False
+        
+    def connectToServer(self):
+        self.service = rpyc.connect(self.host, self.port)
+        self.connected = True
+        
+    def __getattribute__(self, attrName):
+        if (not self.connected):
+            self.connectToServer()
+        return self.service.getAttr(self.id, attrName)
+    
+    def __setattr__(self, attrName, newVal):
+        if (not self.connected):
+            self.connectToServer()
+        self.__dict__[attrName] = self.service.setAttr(self.id, attrName, newVal)
+        
         
     
 class TestQueueService(rpyc.Service):
     '''
-    classdocs
+    A prototype of the v2 edX asynchronous task queue service
+    
+    This service allows putting tasks into a queue, removing those tasks from the queue,
+    and tracking the status and progress of the task as it is worked on. The queue itself 
+    if very simple, being a FIFO queue of Task objects. Associated with each Task is a 
+    TaskStatus object, that is used to store information that is shared between the client,
+    the worker, and the task itself. 
+    
+    The current implementation uses RPYC, a simple rpc mechanism for python clients and
+    servers that are built to run within the same administrative domain.
     '''
 
     def __init__(self):
         '''
-        Constructor
+        Constructor: create an empty queue and dictionary of TaskStatus objects, along with 
+        the information needed to find the service
+        
+        
         '''
+        self.hostname = gethostname()
+        self.socAddr = 19962
         self.queue = deque([])
         self.tStatus = {}
-        
+        print ('Test Queue Service has been started')
     
     def on_connect(self):
         pass
@@ -55,27 +88,38 @@ class TestQueueService(rpyc.Service):
         pass
     
     def exposed_put(self, task):
+        '''
+        Put a task on the queue, returning a TSProxy that allow the client to track task progress
+        
+        
+        '''
         retTS = TaskStatus(task.id, task.destId)
         self.queue.append(task)
         self.tStatus[task.id] = retTS
-        return retTS
+        retProxy = TSProxy(task.id, task.destId, self.hostname, self.socAddr)
+        return retProxy
     
     def exposed_get(self):
         retTask = self.queue.popleft()
-        retTS = TSProxy(self.tStatus[retTask.id], self)
+        self.tStatus[retTask.id].qStatus = TQStatus.taken
+        retTS = TSProxy(retTask.id, retTask.destId, self.hostname, self.socAddr)
         return (retTask, retTS)
     
-    def exposed_get_status(self, taskId):
-        pass
+    def exposed_getAttr(self, taskId, attrName):
+        if (attrName == 'qStatus'):
+            return self.tStatus[taskId].qStatus
+        elif (attrName == 'progress'):
+            return self.tStatus[taskId].progress
     
-    def exposed_cancel_task(self, taskId):
-        pass
-    
-    def exposed_update_progress(self, taskId, progress):
-        pass
-    
-    def exposed_update_taskStatus(self, taskId, newStatus):
-        pass
-    
-    
+    def exposed_setAttr(self, taskId, attrName, newVal):
+        if (attrName == 'qStatus'):
+            self.tStatus[taskId].qStatus = newVal
+        elif (attrName == 'progress'):
+            self.tStatus[taskId].progress = newVal
+        
+
+if __name__ == '__main__':
+    from rpyc.utils.server import ThreadedServer
+    t = ThreadedServer(TestQueueService, port = 19962)
+    t.start()
         
---
+++ TQService/README.md
@@ -0,0 +1,5 @@
+This directory contains a prototype of the Asynchronous Task system for edX. The prototype uses the 
+rpyc package for communication. This code is not meant to be production quality, but rather 
+a proof of concept to test out the interfaces that are being proposed as part of the architectural
+re-work of the platform.
+
